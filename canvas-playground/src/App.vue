c3bb454172b33c1c9122715c18ecaa8dc01d68cf
HTsuyoshi <henriquetsuyoshiy@gmail.com>
2023-05-10 22:06:31 -0300

    fix touch events and add title

--- canvas-playground/src/App.vue
+++ canvas-playground/src/App.vue
@@ -15,14 +15,16 @@
 			h: window.innerHeight
 		}
 		let mouse = {
-			x: -100,
-			y: -100
+			x: - 200,
+			y: - 200
 		}
 		let touch: any[] = [];
+		let ballNum = 100;
 
 		canvas.width  = win.w;
 		canvas.height = win.h;
 
+		// Computer
 		window.addEventListener (
 			'mousemove',
 			(e) => {
@@ -32,43 +34,62 @@
 		)
 
 		window.addEventListener (
+			'click',
+			(e) => {
+				if (e.clientX > win.w/2)
+					for (let i = 0; i < 10; i++)
+						circles.push(createBall());
+				if (e.clientX < win.w/2)
+					circles.splice(0, 10);
+			}
+		)
+
+		// Cellphone
+		window.addEventListener (
 			'touchstart',
 			(e) => {
 				for (let t of e.changedTouches) {
+					if (t.clientX > win.w/2)
+						for (let i = 0; i < 10; i++)
+							circles.push(createBall());
+					if (t.clientX < win.w/2)
+						circles.splice(0, 10);
 					touch.push(t);
 				}
+				ctx.strokeStyle = borderColorStroke;
+				ctx.fillStyle = borderColorFill;
 			}
 		)
 
 		window.addEventListener (
 			'touchmove',
 			(e) => {
-				for (let t of e.changedTouches) {
-					for (let i=0; i<touch.length; ++i) {
-						if (t.target == touch[i].target) {
+				for (let t of e.changedTouches)
+					for (let i=0; i<touch.length; ++i)
+						if (t.target == touch[i].target)
 							touch[i] = t;
-							if (i >= touch.length) break;
-							continue;
-						}
-					}
-				}
 			}
 		)
 
 		window.addEventListener (
 			'touchend',
 			(e) => {
-				for (let t of e.changedTouches) {
-					for (let i=0; i<touch.length; ++i) {
-						if (t.target == touch[i].target) {
-							touch.splice(i, 1);
-							if (i >= touch.length) break;
-							continue;
-						}
-					}
+			for (let t of e.changedTouches) {
+				const index = touch.findIndex(item => item.target === t.target);
+					if (index !== -1) touch.splice(index, 1);
 				}
 			}
 		)
+
+		window.addEventListener(
+			'touchcancel',
+			(e) => {
+			for (let t of e.changedTouches) {
+				const index = touch.findIndex(item => item.target === t.target);
+					if (index !== -1) touch.splice(index, 1);
+				}
+			}
+		);
 
 		window.addEventListener (
 			'resize',
@@ -80,13 +101,14 @@
 			}
 		)
 
-		canvas.style.background = '#000000';
+		canvas.style.background = '#ffffff';
 
 		// Essential functions
 		function getRandom(min: number, max: number): number {
 			return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
 
+		// Shapes
 		interface CircleInterface {
 			x: number;
 			y: number;
@@ -132,21 +154,21 @@
 				if (this.x + this.r > win.w) this.dx = - Math.abs(this.dx);
 				if (this.y - this.r < 0)     this.dy = Math.abs(this.dy);
 				if (this.y + this.r > win.h) this.dy = - Math.abs(this.dy);
-				if (mouse.x - this.x <   50 &&
-					mouse.x - this.x > - 50 &&
-					mouse.y - this.y <   50 &&
-					mouse.y - this.y > - 50) {
-					if (this.r < 30) {
-						this.r += 15;
+				if (mouse.x - this.x <   win.w/20 &&
+					mouse.x - this.x > - win.w/20 &&
+					mouse.y - this.y <   win.w/20 &&
+					mouse.y - this.y > - win.w/20) {
+					if (this.r < Math.max(win.w/20, 75)) {
+						this.r += 10;
 					}
 				} else {
 					for (let t of touch)
 					{
-						if (t.clientX - this.x <   50 &&
-							t.clientX - this.x > - 50 &&
-							t.clientY - this.y <   50 &&
-							t.clientY - this.y > - 50) {
-							if (this.r < 15) {
+						if (t.clientX - this.x <   win.w/20 &&
+							t.clientX - this.x > - win.w/20 &&
+							t.clientY - this.y <   win.w/20 &&
+							t.clientY - this.y > - win.w/20) {
+							if (this.r < Math.max(win.w/20, 75)) {
 								this.r += 10;
 							}
 						}
@@ -161,11 +183,47 @@
 			}
 		}
 
+		function border() {
+			const border = 10;
+			const gap = 5;
+
+			ctx.strokeStyle = borderColorStroke;
+			ctx.fillStyle = borderColorFill;
+			ctx.lineWidth = 2;
+			ctx.strokeRect(border, border, win.w - (2*border), win.h - (2*border));
+
+			ctx.lineWidth = 5;
+			ctx.strokeRect(gap + border, gap + border, win.w - (2*gap + 2*border), win.h- (2*gap + 2* border));
+
+			ctx.lineWidth = 2;
+			ctx.strokeRect(2*gap + border, 2*gap + border, win.w - (2*2*gap + 2*border), win.h - (2*2*gap + 2*border));
+
+			const offset = 3;
+			let title = 'Github';
+			ctx.font = `100px Bebas Neue`;
+			ctx.fillText(title, (win.w/2) - (ctx.measureText(title).width / 2), win.h/2);
+			ctx.strokeText(title, (win.w/2) - offset - (ctx.measureText(title).width / 2), (win.h/2) - offset);
+
+			title = '/Htsuyoshi';
+			ctx.font = `50px Bebas Neue`;
+			ctx.fillText(title, (win.w/2) - (ctx.measureText(title).width / 2), win.h/2 + 50);
+			ctx.strokeText(title, (win.w/2) - offset - (ctx.measureText(title).width / 2), (win.h/2) + 50 - offset);
+
+			title = 'ADD';
+			ctx.font = `30px Bebas Neue`;
+			ctx.fillText(title, (win.w*3/4) - (ctx.measureText(title).width / 2), win.h*1/8 + 50);
+
+			title = 'RM';
+			ctx.font = `30px Bebas Neue`;
+			ctx.fillText(title, (win.w*1/4) - (ctx.measureText(title).width / 2), win.h*1/8 + 50);
+		}
+
 		function drawAnimation(): void {
 			ctx.clearRect(0, 0, win.w, win.h);
-			for (c of circles) {
+			for (let c of circles) {
 				c.update();
 			}
+			border();
 			requestAnimationFrame(drawAnimation);
 		}
 
@@ -176,16 +234,28 @@
 								'#9aeba3',
 								'#dafdba'
 								];
+		const colorsLength = colors.length;
+		const borderColorIndex = getRandom(0, colorsLength - 1);
+		const borderColorStroke = colors[borderColorIndex];
+		const borderColorFill = colors[borderColorIndex + 1 % colorsLength];
+
+		function createBall(): Circle {
+			let dx = getRandom(-2, 2);
+			while (dx == 0) dx = getRandom(-2, 2);
+			let dy = getRandom(-2, 2);
+			while (dy == 0) dy = getRandom(-2, 2);
+			const { x, y, radius, color } = {
+				x: getRandom(0, win.w),
+				y: getRandom(0, win.h),
+				radius: getRandom(5, 5),
+				color: colors[getRandom(0, colorsLength - 1)]
+			};
+			return new Circle(x, y, dx, dy, radius, color);
+		}
 
 		let circles: Array<Circle> = [];
-		for (let i = 0; i < 500; i++) {
-			var c = new Circle(getRandom(0, win.w),
-								getRandom(0, win.h),
-								getRandom(-2, 2),
-								getRandom(-2, 2),
-								getRandom(5, 5),
-								colors[getRandom(0, colors.length - 1)]);
-			circles.push(c);
+		for (let i = 0; i < ballNum; i++) {
+			circles.push(createBall());
 		}
 
 		drawAnimation();
@@ -197,4 +267,5 @@
 </template>
 
 <style scoped>
+	@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
 </style>
